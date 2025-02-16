c3b67a920a3fd905bd5eaeda641407fc72c96465
Gouravsen1202 <gouravsen757@gmail.com>
2025-02-16 23:36:44 +0530

    new code

---
+++ styling in react/src/App.jsx
@@ -0,0 +1,32 @@
+import myimg from "./images/ramm.jpeg"
+import myimg2 from "./images/ramm.jpeg"
+import myimg3 from "./images/ramm.jpeg"
+
+const App=()=>{
+  return(
+ <>
+ {/* <body className="hii">
+  red
+ </body>
+
+ <h1> blue</h1>
+ 
+
+ <h3>green</h3> */}
+
+
+ 
+ < img src={myimg}
+ />
+ <
+ img src={myimg2}
+ />
+ <
+ img src={myimg3}
+ />
+ <img src="public/images/hanuman.jpeg"  />
+ </>
+
+  )
+}
+export default App;
